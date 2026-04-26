27b217519a7c9a9bb4ad28e86322541689b00f22
camiladacol <camia.dacol@correo.ucu.edu.uy>
2026-04-26 12:51:40 -0300

    Ejercicio 6 - Parte 1,2,3,4

---
+++ src/main/java/org/example/Ejercicio6/ArbolBinario.java
@@ -0,0 +1,84 @@
+package org.example.Ejercicio6;
+
+import java.util.function.Consumer;
+
+public class ArbolBinario <T> implements TDAArbolBinario {
+    private TDAElemento <T> raiz;
+    public ArbolBinario() {
+        this.raiz = null;
+    }
+
+    @Override
+    public Object buscar(Comparable predicate) {
+        return null;
+    }
+
+    @Override
+    public TDAElemento obtenerRaiz() {
+        return raiz;
+    }
+
+    @Override
+    public boolean eliminar(Comparable criterioBusqueda) {
+        return false;
+    }
+
+    @Override
+    public boolean insertar(Comparable dato) {
+        return false;
+    }
+
+    @Override
+    public void inOrder(Consumer consumidor) {
+    if (raiz != null) {
+        raiz.inOrder(consumidor);
+    }
+    }
+
+    @Override
+    public void preOrder(Consumer consumidor) {
+
+    }
+
+    @Override
+    public void postOrder(Consumer consumidor) {
+
+    }
+
+    @Override
+    public boolean esVacio() {
+        return false;
+    }
+
+    @Override
+    public int cantidadNodos() //Punto 2 del ejercicio (tamaño)
+    {
+        if (raiz == null) {
+            return 0;
+        }
+        return raiz.cantidadNodos();
+    }
+
+    @Override
+    public int cantidadHojas() //Punto 3 del ejercicio (hojas)
+    {
+        if (raiz == null) {
+            return 0;
+        }
+        return raiz.cantidadHojas();
+    }
+
+    @Override
+    public int cantidadNodosInternos() //Punto 4 del ejercicio (internos)
+    {
+        if (raiz == null) {
+            return 0;
+        }
+        return raiz.cantidadNodosInternos();
+    }
+    public int altura () //Punto 1 del ejercicio
+    {
+        if (raiz == null) return -1;
+        return raiz.altura();
+    }
+}
---
+++ src/main/java/org/example/Ejercicio6/ElementoArbol.java
@@ -0,0 +1,182 @@
+package org.example.Ejercicio6;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ElementoArbol <T> implements TDAElemento <T> { //Implementación de la interfaz
+
+    private T dato; //Dato que guarda el nodo
+    private TDAElemento <T> hijoIzquierdo; //Referencia al hijo izquierdo
+    private TDAElemento <T> hijoDerecho; //Referencia al hijo derecho
+
+    public ElementoArbol(T dato) { //Constructor
+        this.dato = dato;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = null;
+    }
+
+    @Override
+    public void setHijoIzquierdo(TDAElemento<T> hijoIzquierdo) { //Asigna hijo izquierdo
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    @Override
+    public void setHijoDerecho(TDAElemento<T> hijoDerecho) { //Asigna hijo derecho
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    @Override
+    public TDAElemento<T> getHijoIzquierdo() { //Devuelve hijo izquierdo
+        return hijoIzquierdo;
+    }
+
+    @Override
+    public TDAElemento<T> getHijoDerecho() { //Devuelve hijo derecho
+        return hijoDerecho;
+    }
+
+    @Override
+    public void setDato(T dato) { //Cambia el dato guardado
+        this.dato = dato;
+    }
+
+    @Override
+    public T getDato() { //Devuelve el dato guardado
+        return dato;
+    }
+
+    @Override
+    public TDAElemento<T> buscar(Comparable<T> criterioBusqueda) {
+        return null;
+    }
+
+    @Override
+    public TDAElemento<T> eliminar(Comparable<T> criterioBusqueda) {
+        return null;
+    }
+
+    @Override
+    public boolean insertar(Comparable<T> nuevoDato) {
+        return false;
+    }
+
+    @Override
+    public void inOrder(Consumer<TDAElemento<T>> consumidor) {
+        if (hijoIzquierdo != null) {
+            hijoIzquierdo.inOrder(consumidor);
+        }
+        consumidor.accept(this);
+        if (hijoDerecho != null) {
+            hijoDerecho.inOrder(consumidor);
+        }
+    }
+
+    @Override
+    public void preOrder(Consumer<TDAElemento<T>> consumidor) {
+        consumidor.accept(this);
+        if (hijoIzquierdo != null) {
+            hijoIzquierdo.preOrder(consumidor);
+        }
+        if (hijoDerecho != null) {
+            hijoDerecho.preOrder(consumidor);
+        }
+    }
+
+    @Override
+    public void postOrder(Consumer<TDAElemento<T>> consumidor) {
+        if (hijoIzquierdo != null) {
+            hijoIzquierdo.postOrder(consumidor);
+        }
+        if (hijoDerecho != null) {
+            hijoDerecho.postOrder(consumidor);
+        }
+        consumidor.accept(this);
+    }
+
+    @Override
+    public boolean esHoja() { //Verifica si el nodo es hoja, es hoja si no tiene hijos
+        return hijoIzquierdo == null && hijoDerecho == null;
+    }
+
+    @Override
+    public int cantidadHojas() //Punto 3 del ejercicio (hojas)
+    {
+        if (esHoja()) //Caso base
+        {
+            return 1;
+        }
+        int hojas = 0;
+        if (hijoIzquierdo != null) //Suma hojas del subarbol izquierdo
+        {
+            hojas += hijoIzquierdo.cantidadHojas();
+        }
+        if (hijoDerecho != null) //Suma hojas del subarbol derecho
+        {
+            hojas += hijoDerecho.cantidadHojas();
+        }
+        return hojas;
+    }
+
+    @Override
+    public int cantidadNodosInternos() //Punto 4 del ejercicio (internos)
+    {
+       if (esHoja()) //Caso base, las hojas no son nodos internos
+       {
+           return 0;
+       }
+       int total = 1;
+       if (hijoIzquierdo != null)
+       {
+           total+= hijoIzquierdo.cantidadNodosInternos();
+       }
+       if (hijoDerecho != null)
+       {
+           total+= hijoDerecho.cantidadNodosInternos();
+       }
+       return total;
+    }
+
+    @Override
+    public int cantidadNodos() //Punto 2 del ejercicio (tamaño)
+    {
+        int total = 1;
+        if (hijoIzquierdo != null) //Suma nodos del subarbol izquierdo
+        {
+            total += hijoIzquierdo.cantidadNodos();
+        }
+        if (hijoDerecho != null) //Suma nodos del subarbol derecho
+        {
+            total += hijoDerecho.cantidadNodos();
+        }
+        return total;
+    }
+
+    @Override //Punto 1 del ejercicio
+    public int altura() { //Calcula la altura del nodo
+        int alturaIzquierda = -1; //Altura del subarbol izquierdo
+        int alturaDerecha = -1; //Altura del subarbol derecho
+
+        if (hijoIzquierdo != null) { //Si existe hijo izquierdo, calcula la altura
+            alturaIzquierda = hijoIzquierdo.altura();
+        }
+        if (hijoDerecho != null) { //Si existe hijo derecho, calcula la altura
+            alturaDerecha = hijoDerecho.altura();
+        }
+        return Math.max(alturaIzquierda,alturaDerecha) +1 ;
+    }
+
+    @Override
+    public int obtenerNivel(Comparable<T> criterioBusqueda) {
+        if (criterioBusqueda.compareTo(this.dato) == 0) return 0;
+
+        if (hijoIzquierdo != null) {
+            int nivel = hijoIzquierdo.obtenerNivel(criterioBusqueda);
+            if (nivel != -1) return nivel + 1;
+        }
+        if (hijoDerecho != null) {
+            int nivel = hijoDerecho.obtenerNivel(criterioBusqueda);
+            if (nivel != -1) return nivel + 1;
+        }
+        return -1;
+    }
+}
